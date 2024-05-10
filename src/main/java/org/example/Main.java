ac3e6e697b3f9f4019c7140ea4d16d647d3564fd
Nast_programmer <anastypol33@gmail.com>
2024-05-10 21:30:55 +0300

    second task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,16 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -20,6 +30,9 @@ public class Main {
         List<Employee> list = parseCSV(columnMapping, fileName);
         String json = listToJson(list);
         writeString(json);
+        List<Employee> list1 = parseXML("data.xml");
+        String json1 = listToJson(list1);
+        writeString(json1);
     }
 
     public static List<Employee> parseCSV(String[] arr, String fileName) {
@@ -50,5 +63,33 @@ public class Main {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public static List<Employee> parseXML(String fileName) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(new File(fileName));
+            Node root = doc.getDocumentElement();
+            NodeList nodeList = root.getChildNodes();
+            List<Employee> employees = new ArrayList<>();
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element el = (Element) node;
+                    int id = Integer.parseInt(el.getElementsByTagName("id").item(0).getTextContent());
+                    String firstName = el.getElementsByTagName("firstName").item(0).getTextContent();
+                    String lastName = el.getElementsByTagName("lastName").item(0).getTextContent();
+                    String country = el.getElementsByTagName("country").item(0).getTextContent();
+                    int age = Integer.parseInt(el.getElementsByTagName("age").item(0).getTextContent());
+                    Employee employee = new Employee(id, firstName, lastName, country, age);
+                    employees.add(employee);
+                }
+            }
+            return employees;
+        } catch (IOException | SAXException | ParserConfigurationException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 }
